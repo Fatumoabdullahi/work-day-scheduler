f768ae199d09e3ebecdb6ed84b07427f163853da
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-22 22:27:09 +0000

    sibling and parent html changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
     $("#currentDay").text(moment ().format("MMMM Do YYYY, h:mm:ss a"));
+    $(".saveBtn").on("click" function () {
+        console.log(this);
+        var text = $(this).siblings(".descriptions").val();
+        var time = $(this).parent().attr("id");
+    })
 }
     )
