07c6d9a7e6374ae7210f5aefac854b474d83ad91
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-22 23:00:51 +0000

    moment.js

--- Develop/script.js
+++ Develop/script.js
@@ -19,4 +19,9 @@ $(document).ready(function () {
     $("#hour15.description").val(localStorage.getItem("hour15"));
     $("#hour16.description").val(localStorage.getItem("hour16"));
     $("#hour17.description").val(localStorage.getItem("hour17"));
+
+    function hourTracker () {
+        //collect current hours
+        var currentHour = moment().hour();
+    }
 })
